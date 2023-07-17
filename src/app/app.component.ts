64ca0e34a917e26ba433c9fee5ab0b1a7d34ce04
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-17 10:32:27 -0400

    Add SSR and test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ssr-test';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    console.warn(document.querySelector('[random-attribute]'));
+  }
 }
